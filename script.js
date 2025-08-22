fc0be8288bd68da9d35bab4483209e78d8530df4
Leozera2k <leofsousa98@gmail.com>
2025-08-22 11:34:24 -0300

    criação script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const nota = 9;
+
+nota > 5 ? console.log('Aprovado') : console.log("Reprovado")
+
+
+
+
+
+
+// && = and(e)
+// || = or(ou)
+// ( == === ) = IGUAL
+// != !== = diferente
+// > = maior que
+// < = menor que
